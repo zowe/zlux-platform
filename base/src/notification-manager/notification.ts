be394e02e9e527f831e832841551e65269139377
Jordan Filteau <jfilteau@rocketsoftware.com>
2018-09-06 20:06:38 -0400

    Finished implementing notification framework
    Signed-off-by: Jordan Filteau <jfilteau@rocketsoftware.com>

--- base/src/notification-manager/notification.ts
+++ base/src/notification-manager/notification.ts
@@ -3,14 +3,14 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
 
-export class Notification {
+export class Notification implements MVDHosting.NotificationInterface {
   private message: string;
   private date: Date;
   private type: MVDHosting.NotificationType;
@@ -39,9 +39,8 @@ export class Notification {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
-
