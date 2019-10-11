3083fb0acc5137abb5669739c503a23fae238104
Timothy Gerstel <tgerstel@rocketsoftware.com>
2019-10-11 15:09:58 -0400

    Change type to promise
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- interface/src/index.d.ts
+++ interface/src/index.d.ts
@@ -125,8 +125,8 @@ declare namespace ZLUX {
   interface Globalization {
     getLanguage(): string;
     getLocale(): string;
-    setLanguage(language: string): Promise<any>;
-    setLocale(locale: string): Promise<any>;
+    setLanguage(language: string): any;
+    setLocale(locale: string): any;
   }
 
   type UnixFileUriOptions = {
