8d1d45a6f92b81e4cb7f1c065a56506c35909fce
Timothy Gerstel <tgerstel@rocketsoftware.com>
2021-02-03 23:59:17 -0500

    Formatting
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- base/src/dispatcher/dispatcher.ts
+++ base/src/dispatcher/dispatcher.ts
@@ -361,7 +361,7 @@ export class Dispatcher implements ZLUX.Dispatcher {
      this.recognizers.push(recognitionRule);
      if (predicate.operation == RecognitionOp.AND){
        for (let subClause of predicate.subClauses){
-        const operation = (subClause as RecognitionClause).operation;
+         const operation = (subClause as RecognitionClause).operation;
          if (operation === RecognitionOp.PROPERTY_EQ || operation === RecognitionOp.PROPERTY_NE){
            let propertyClause:RecognitionClause = subClause as RecognitionClause;
            let propertyName:string = propertyClause.subClauses[0] as string;
